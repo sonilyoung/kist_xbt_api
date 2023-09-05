50da34845e52b5abd19e46eb470b2185f9b6ec17
sonilyoung <iys4906@naver.com>
2023-09-06 00:23:18 +0900

    ㅅㅅ

---
+++ src/main/java/egovframework/com/file/service/FileStorageService.java
@@ -0,0 +1,38 @@
+package egovframework.com.file.service;
+
+import java.io.File;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import egovframework.com.api.edc.vo.XrayImgContents;
+import egovframework.com.file.vo.AttachFile;
+
+
+/**
+ * 파일 Storage 관련 Service
+ * 
+ * @fileName : FileStorageService.java
+ * @author : YeongJun Lee
+ * @date : 2022.07.12
+ */
+public interface FileStorageService {
+	
+	AttachFile createUnitImageFile(String targetName, AttachFile params ,MultipartFile file) throws Exception;
+
+	AttachFile createXrayImageFile(String targetName, AttachFile params ,MultipartFile file) throws Exception;
+	
+	AttachFile createXrayImageFiles(String targetName, String fileNameWithoutExtension, XrayImgContents params,MultipartFile file) throws Exception;
+	
+	AttachFile createKistXrayImageFiles(String targetName, String fileNameWithoutExtension, XrayImgContents params, MultipartFile file) throws Exception;
+		
+    File getFile(AttachFile AttachFile);
+
+    AttachFile createFile(MultipartFile file) throws Exception;
+
+    boolean deleteFile(AttachFile AttachFile);
+    
+    void getImage(String filePath, HttpServletResponse response) throws Exception;
+
+}
